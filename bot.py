b664eb6232a1beb97539aa15ed25d5d2e6bc7835
mehrabkb <mkordbacheh75@gmail.com>
2024-01-26 20:36:04 +0330

    create txt file and write link to it

--- bot.py
+++ bot.py
@@ -2,14 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-
-
 driver = webdriver.Firefox()
 driver.get('https://aparat.com/mehrabkb')
 
 links = driver.find_elements(By.CSS_SELECTOR , 'a[class~="titled-link"]')
 time.sleep(20)
 target_links = []
+print(links)
+f = open("links.txt" , "w") 
 for link in links:
     target_links.append(link.get_attribute("href"))
+    f.write(link.get_attribute("href") + '\n')
+f.close()
 print(len(target_links))
