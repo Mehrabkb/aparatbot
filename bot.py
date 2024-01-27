cf106f812041479a9c9b64fe9d4e1090fe85a148
mehrabkb <mkordbacheh75@gmail.com>
2024-01-27 15:12:51 +0330

    beautify code

--- bot.py
+++ bot.py
@@ -5,13 +5,23 @@ import time
 driver = webdriver.Firefox()
 driver.get('https://aparat.com/mehrabkb')
 
-links = driver.find_elements(By.CSS_SELECTOR , 'a[class~="titled-link"]')
-time.sleep(20)
-target_links = []
-print(links)
-f = open("links.txt" , "w") 
-for link in links:
-    target_links.append(link.get_attribute("href"))
-    f.write(link.get_attribute("href") + '\n')
-f.close()
-print(len(target_links))
+# this function to get elements by css selector 
+def getElementsByCssSelector(selector=""):
+    return driver.find_elements(By.CSS_SELECTOR , selector)
+# this function to scroll page with counter and default counter = 3
+def scrollPage(counter = 3):
+    for i in range(0 , counter):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(6)
+
+# this function get links and write to txt file 
+def writeLinksInTxtFile(links):
+    f = open("links.txt" , "w")
+    for link in links : 
+        f.write(link.get_attribute("href") + '\n')
+    f.close()    
+
+scrollPage(5)
+links = getElementsByCssSelector('a[class~="titled-link"]')
+writeLinksInTxtFile(links)
+
