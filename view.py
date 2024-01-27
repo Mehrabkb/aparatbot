043de4e8380b849c471d461e91b22e875b881c5c
mehrabkb <mkordbacheh75@gmail.com>
2024-01-27 18:44:28 +0330

    view py worked

---
+++ view.py
@@ -0,0 +1,22 @@
+from selenium import webdriver 
+import time
+
+driverList = []
+def view():
+    f = open("links.txt" , "r")
+    count = 0 
+    for line in f.readlines():
+        driver = webdriver.Firefox()
+        driver.get(line)
+        driverList.append(driver)
+        print(driverList)
+        time.sleep(4)     
+    f.close()
+
+def closeAllDrivers():
+    for d in driverList:
+        d.close()
+
+view()
+time.sleep(60)
+closeAllDrivers()
